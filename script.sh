c1cc5d9e2cf73031096dc4fa9f7b15fed7095928
Dounia <hajjajidounia8@gmail.com>
2025-03-03 13:28:25 +0000

    doctype

---
+++ script.sh
@@ -0,0 +1,28 @@
+# loop
+# read -p "guess a number : " number
+
+# while [[ $number != 8 ]]; do
+
+# read -p "guess a number : " number
+# if [[ $number == 8 ]]; then
+# echo "congrats"
+# elif [[ $number -gt 8 ]]; then
+# echo "kbare mane ra9ame"
+# elif [[ $number -lt 8 ]]; then
+# echo "s4are mane ra9ame"
+# fi
+# done
+# read -p "guess anumber :" number
+# while [[ $number != 10 ]]; do
+# if [[ $number == 10 ]]; then
+
+# read -p "guess anumber :" number
+# elif [[ $number -gt 10 ]]; then
+# echo "pls grand que"
+# read -p "guess anumber :" number 
+# elif [[ $number -lt 10 ]]; then
+# echo "plus petit que"
+# read -p "guess anumber :" number 
+# fi
+# done
+# echo "congrats"
